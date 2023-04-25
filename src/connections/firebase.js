c9e931d38e66297a5a001b6e71611f965fec641f
rohanyrane <ryrane.in@gmail.com>
2023-04-26 00:17:16 +0530

    Firebase

---
+++ src/connections/firebase.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEJ8falK20SJUmWkXDnkG4mdBbmanmcnM",
+  authDomain: "smart-parking-b1.firebaseapp.com",
+  projectId: "smart-parking-b1",
+  storageBucket: "smart-parking-b1.appspot.com",
+  messagingSenderId: "554387139632",
+  appId: "1:554387139632:web:21a45be5fed2b26dc611f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
